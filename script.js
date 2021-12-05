5274f8f3bbe9a5c6ea8449ad3c100fd01a1b1448
ccm731 <95583716+ccm731@users.noreply.github.com>
2021-12-05 16:08:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,15 @@ function compute()
     p = document.getElementById("principal").value;
     
 }
+var principal = document.getElementById("principal") .value;
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  principal * years * rate / 100
+  var interest = princiapl * years * rate / 100;
+  var year = new Date().getFullYear()+parseInt(years);
+      funtion updateRate()
+          {
+              var rateval = document,getElementById("rate").value;
+          document.getElementById("rate_val").innerText=rateval;
+          }
         
